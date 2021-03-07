b17cfe89371c21b66f4199d7b822ed236bf7b9dd
JunYoung <702wnsdud@gmail.com>
2021-03-07 19:22:25 +0900

    Josephus

---
+++ algospot/Josephus.java
@@ -0,0 +1,37 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Josephus {
+
+    public static void main(String[] args) {
+            
+
+            int n, m, c, k;
+            Scanner sc = new Scanner(System.in);
+
+            c = sc.nextInt();
+
+            for (int _c = 0; _c < c; _c++) {
+                        LinkedList<Integer> l = new LinkedList<>();
+                        n = sc.nextInt();
+                        m = sc.nextInt();
+
+                        for (int i = 0; i < n; i++) {
+                                        l.add(i + 1);
+                                    }
+
+                        k = 0;
+
+                        for (int i = 0; i < n - 2; i++)
+                        {
+                                        l.remove(k);
+                                        k = (k + m - 1) % l.size();
+                                    }
+                        int x = l.get(0);
+                        int y = l.get(1);
+
+                        if (x > y) System.out.println(y + " " + x);
+                        else System.out.println(x + " " + y);
+                    }
+        }
+}
